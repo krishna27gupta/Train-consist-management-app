40548d76996e309fc7b23f4c83648737c4f120f8
krishna27gupta <Krishna27gupta06@gmail.com>
2026-04-27 15:45:04 +0530

    Added UC8 - Stream filtering for bogies based on capacity

---
+++ TRAINMANAGAMENTAPP/src/uc8.java
@@ -0,0 +1,25 @@
+package main;
+
+public class UseCase9Bogie {
+
+    private String name;
+    private int capacity;
+
+    public UseCase9Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Bogie{name='" + name + "', capacity=" + capacity + "}";
+    }
+}
